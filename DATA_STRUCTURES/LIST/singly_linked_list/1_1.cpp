35f78fc6bfaa828151048434ff459151e0064fcb
KamilPawlaak <kamil2137@poczta.fm>
2025-04-08 18:09:14 +0200

    add Destroy(), Size(), push_back()

--- DATA_STRUCTURES/LIST/singly_linked_list/1_1.cpp
+++ DATA_STRUCTURES/LIST/singly_linked_list/1_1.cpp
@@ -29,6 +29,27 @@ void push_front(Node_list*& head, int value){
     head=newElemnet;
 }
 
+void push_back(Node_list*& head, int value){
+
+    Node_list* newElement = new Node_list;
+    newElement->data=value;
+    newElement->next=nullptr;
+
+
+    if(head==nullptr){
+        head=addFirstElement(value);
+        return;
+    }
+
+    Node_list* temp = head;
+    while(temp->next!=nullptr){
+        temp=temp->next;
+    }
+
+    temp->next=newElement;
+
+}
+
 void Print(Node_list* head){
 
     while (head!=nullptr)
@@ -36,8 +57,31 @@ void Print(Node_list* head){
         cout<<"| "<<head->data<<" | --> ";
         head=head->next;
     }
-    cout<<endl;
+    cout<<"NULL"<<endl;
 
+}
+
+void Destroy(Node_list*& head){
+
+    
+    while(head!=nullptr){
+        Node_list* temp = head;
+        head=head->next;
+        delete temp;
+    }
+}
+
+int Size(Node_list*& head){
+
+    int counter = 0;
+    while (head!=nullptr)
+    {
+        head=head->next;
+        counter++;
+        
+    }
+    
+    return counter;
 }
 
 int main(){
@@ -47,6 +91,10 @@ int main(){
     push_front(head,130);
     push_front(head,50);
     push_front(head,222);
+    push_back(head,44);
+    //Destroy(head);
+    
     Print(head);
+    cout<<"Size list: "<<Size(head)<<endl;
 
 }
