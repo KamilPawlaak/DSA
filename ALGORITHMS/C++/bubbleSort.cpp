1f03ccd5e92856739f64eea5446b27ed271b1a0d
KamilPawlaak <kamil2137@poczta.fm>
2025-04-08 23:14:10 +0200

    buubleSort.cpp

---
+++ ALGORITHMS/C++/bubbleSort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void BubbleSort(int* tab, int size){
+
+    for(int i =0; i<size-1;i++){
+        for(int j =0; j<size-i-1;j++){
+            if(tab[j]>tab[j+1]){
+                int temp = tab[j];
+                tab[j]=tab[j+1];
+                tab[j+1]=temp;
+            }
+
+        }
+    }
+
+}
+
+
+int main(){
+
+    int tab[]={3,12,2,45,1333,54,23};
+    int size = sizeof(tab)/sizeof(tab[0]);
+
+    
+    cout<<"Before: "<<endl;
+    for(int i : tab){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    cout<<"After: "<<endl;
+    BubbleSort(tab,size);
+    for(int i : tab){
+        cout<<i<<" ";
+    }
+
+
+
+
+}
