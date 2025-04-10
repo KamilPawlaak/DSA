e76a434b90df4880ffc9807532e5b7c4ae313cb9
KamilPawlaak <kamil2137@poczta.fm>
2025-04-10 18:29:35 +0200

    new function

--- ALGORITHMS/C++/select_sort.cpp
+++ ALGORITHMS/C++/select_sort.cpp
@@ -1,15 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int tab[] = {21, 75, 32, 2, 6, 2, 19,333};
-    int size = sizeof(tab) / sizeof(tab[0]);
-
-    cout << "Before: " << endl;
-    for (int i : tab) {
-        cout << i << " ";
-    }
-    cout << endl;
+void selectSort(int* tab, int size){
 
     for (int i = 0; i < size - 1; i++) {
         int min_index = i;
@@ -23,12 +15,28 @@ int main() {
         tab[i] = tab[min_index];
         tab[min_index] = temp;
     }
+}
 
-    cout << "After: " << endl;
-    for (int i : tab) {
-        cout << i << " ";
+void showTab(int* tab, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << tab[i] << " ";
     }
     cout << endl;
+}
+
+
+int main() {
+    int tab[] = {21, 75, 32, 2, 6, 2, 19,333};
+    int size = sizeof(tab) / sizeof(tab[0]);
+
+    cout << "Before: " << endl;
+    showTab(tab, size);
+
+
+    selectSort(tab, size);
+
+    cout << "After: " << endl;
+    showTab(tab, size);
 
     return 0;
 }
